28862c723ae7751b41f3c8dccc997e56f5b17f06
Eugene Shulepin <e.shulepin@semrush.com>
2015-10-27 16:41:17 +0300

    [smd] implemented required attributes for swagger

--- generator/SmdToSwaggerConverter.php
+++ generator/SmdToSwaggerConverter.php
@@ -86,9 +86,11 @@
                 'definitions' => [
                     'JsonRpcErrorInner' => [
                         'properties' => self::$baseJsonRpcErrorInner,
+                        'required'   => [ 'code', 'message' ],
                     ],
                     'JsonRpcError'      => [
                         'properties' => self::$baseJsonRpcError,
+                        'required'   => [ 'jsonrpc', 'error', 'id' ],
                     ],
                 ],
             ];
@@ -124,8 +126,8 @@
                     [
                         'name'     => 'params',
                         'in'       => 'body',
-                        'required' => false,
-                        'schema'   => [ '$ref' => '#/definitions/' . $requestRef, 'type' => 'string', ],
+                        'required' => true,
+                        'schema'   => [ '$ref' => '#/definitions/' . $requestRef, ],
                     ],
                 ],
                 'responses'   => [
@@ -147,15 +149,17 @@
             if ( $service->parameters ) {
                 $request['params'] = [ '$ref' => '#/definitions/' . $requestRefInner ];
                 foreach ( $service->parameters as $parameter ) {
-                    $this->parseParameter( $requestRefInner, $parameter, true );
+                    $this->parseParameter( $requestRefInner, $parameter );
                 }
             }
 
-            //slush request
+            //flush request
             $this->swagger['definitions'][$requestRef]['properties'] = $request;
+            $this->swagger['definitions'][$requestRef]['required']   = [ 'jsonrpc', 'method', 'id' ];
 
             //building base response
             $this->swagger['definitions'][$responseRef]['properties'] = self::$baseJsonRpcResponse;
+            $this->swagger['definitions'][$responseRef]['required']   = [ 'jsonrpc', 'result', 'id' ];
 
             //inject existing definitions
             if ( !empty( $service->returns->definitions ) ) {
@@ -181,12 +185,12 @@
          * parse smd parameter to swagger
          * @param string $ref
          * @param Object $parameter
-         * @param bool   $isRequest
          */
-        private function parseParameter( $ref, $parameter, $isRequest ) {
+        private function parseParameter( $ref, $parameter ) {
             $type      = !empty( $parameter->type ) ? $parameter->type : 'string';
             $name      = !empty( $parameter->name ) ? $parameter->name : '';
             $list_type = '';
+            $required  = [ ];
 
             if ( substr( $type, strlen( $type ) - 2, 2 ) === '[]' ) {
                 $list_type = substr( $type, 0, strlen( $type ) - 2 );
@@ -209,17 +213,19 @@
             $parameterParsed = array_filter( [
                 'type'    => $type,
                 'default' => !empty( $parameter->default ) ? $parameter->default : null,
-//                'required' => empty( $parameter->optional ), TODO
             ] );
 
             if ( $list_type ) {
                 $parameterParsed['items'] = [ 'type' => $list_type ];
             }
 
-            if ( $isRequest ) {
-                $this->swagger['definitions'][$ref]['properties'][$name] = $parameterParsed;
-            } else {
-                $this->swagger['definitions'][$ref]['properties']['result'] = $parameterParsed;
+            if ( $parameter->optional ) {
+                $required[] = $name;
+            }
+
+            $this->swagger['definitions'][$ref]['properties'][$name] = $parameterParsed;
+            if ( $required ) {
+                $this->swagger['definitions'][$ref]['required'] = $required;
             }
         }
     }
